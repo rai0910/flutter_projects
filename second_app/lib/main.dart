7b9fdaa0510fdb48619f00a87186f7986493e15d
rai0910 <aditya0910rai@gmail.com>
2024-09-11 01:04:53 +0530

    first commit

---
+++ second_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: (LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 53, 5, 136),
+              Color.fromARGB(255, 78, 7, 200),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ))),
+          child: Center(
+            child: Text(
+              'Hello World',
+              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
