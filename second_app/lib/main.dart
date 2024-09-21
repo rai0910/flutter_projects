3fe3925e4de4562e459ced8bc22b972cca4e495b
rai0910 <aditya0910rai@gmail.com>
2024-09-21 23:16:48 +0530

    cb app revert

--- second_app/lib/main.dart
+++ second_app/lib/main.dart
@@ -11,57 +11,6 @@ void main() async {
   runApp(const CBApp());
 }
 
-class CBApp extends StatelessWidget {
-  static GlobalKey flutterMapKey = GlobalKey();
-  static GlobalKey<ScaffoldState> cbAppKey = GlobalKey();
-
-  static String currentPlattform = "unknown";
-
-  const CBApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider<ModelMapData>(create: (context) => ModelMapData()),
-        ],
-        child: Consumer<ModelMapData>(builder: (context, modelMap, child) {
-          return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'CB App',
-              theme: FlexThemeData.light(
-                scheme: FlexScheme.greenM3,
-                // useMaterial3: true,
-              ),
-              darkTheme: FlexThemeData.dark(
-                scheme: FlexScheme.greenM3,
-                // useMaterial3: true,
-              ),
-              themeMode: ThemeMode.system,
-              locale: modelMap.currentLocale, // const Locale("de", "DE"),
-              localizationsDelegates: AppLocalizations.localizationsDelegates,
-              // supportedLocales: AppLocalizations.supportedLocales,
-
-              // localizationsDelegates: const [
-              //   GlobalMaterialLocalizations.delegate,
-              //   GlobalWidgetsLocalizations.delegate,
-              //   GlobalCupertinoLocalizations.delegate,
-              // ],
-              supportedLocales: const [
-                Locale('en'), // English
-                Locale('de'), // German
-              ],
-              initialRoute: "/",
-              routes: {
-                "/": (context) => const CBAppMain(title: 'Freie Lastenradl'),
-                "/bookings": (context) => const BookingsPage(),
-                "/settings": (context) => const SettingsPage(),
-              });
-        }));
-  }
-}
-
 void main() {
   runApp(
       MaterialApp(
